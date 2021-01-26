f1cf03f04ac634670f95ede0e4513f09bac4b650
code300 <750737873@qq.com>
2021-01-26 20:32:51 +0800

    update home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -56,6 +56,43 @@
         </ul>
       </div>
     </div>
+    <div class="main">
+      <div class="one">
+        <p class="item">
+          <span class="type">手表</span>
+          <a>
+            <span class="more"
+              >查看更多
+              <span class="arrorRight">>></span>
+            </span>
+          </a>
+        </p>
+        <ul>
+          <li v-for="item in productOne" :key="item.id">
+            <a>
+              <img :src="item.img" alt="" />
+              <p class="title">{{ item.title }}</p>
+              <p class="content">
+                {{ item.content }}
+              </p>
+              <p class="price">{{ item.price }}</p>
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li v-for="item in productOne" :key="item.id">
+            <a>
+              <img :src="item.img" alt="" />
+              <p class="title">{{ item.title }}</p>
+              <p class="content">
+                {{ item.content }}
+              </p>
+              <p class="price">{{ item.price }}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -95,6 +132,90 @@ export default {
           id: 3,
           img:
             "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5d02e4b6ad0586f61339a042bacb1c45.jpg?w=2452&h=920",
+        },
+      ],
+      productOne: [
+        {
+          id: 1,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 2,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 3,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 4,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 5,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+      ],
+      productTwo: [
+        {
+          id: 1,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 2,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 3,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 4,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
+        },
+        {
+          id: 5,
+          img:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg",
+          title: "小米11",
+          content: "2k888,四曲面屏",
+          price: "￥5999",
         },
       ],
     };
@@ -218,6 +339,79 @@ export default {
           margin: 14px 0;
           img {
             height: 170px;
+          }
+        }
+      }
+    }
+  }
+  .main {
+    width: 100%;
+    background: #ededed;
+    .one {
+      width: 1226px;
+      margin: 0 auto;
+      // border: 1px solid red;
+      .item {
+        margin: 0;
+        font-size: 22px;
+        font-weight: 200;
+        line-height: 58px;
+        color: #333;
+        text-align: left;
+        margin-bottom: -10px;
+        display: flex;
+        justify-content: space-between;
+        .type {
+          text-align: left;
+        }
+        .more {
+          text-align: right;
+          font-size: 14px;
+        }
+        .arrorRight {
+          padding: 4px;
+          border-radius: 16px;
+          font-size: 12px;
+          line-height: 12px;
+          vertical-align: 1px;
+          -webkit-transition: all 0.4s;
+          transition: all 0.4s;
+          font-weight: 500;
+        }
+      }
+      ul {
+        display: flex;
+        justify-content: space-between;
+        margin: 10px 0;
+        li {
+          float: left;
+          width: 234px;
+          background: #fff;
+          a {
+            img {
+              width: 160px;
+              margin-top: 15px;
+            }
+            h4 {
+              margin: 15px;
+            }
+            p {
+              margin: 15px;
+              font-size: 12px;
+            }
+            .title {
+              color: #333;
+              font-size: 14px;
+              font-weight: 500;
+            }
+            .content {
+              color: #b0b0b0;
+            }
+            .price {
+              color: #ff6700;
+              font-size: 16px;
+              font-weight: 500;
+            }
           }
         }
       }
